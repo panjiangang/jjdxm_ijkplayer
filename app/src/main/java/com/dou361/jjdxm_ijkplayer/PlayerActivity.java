73394151932012ae30274e3b86f7dd9739b4f7b0
402792989 <402792989@qq.com>
2018-03-09 20:50:41 +0800

    Initial commit

---
+++ app/src/main/java/com/dou361/jjdxm_ijkplayer/PlayerActivity.java
@@ -0,0 +1,97 @@
+package com.dou361.jjdxm_ijkplayer;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.dou361.ijkplayer.listener.OnPlayerBackListener;
+import com.dou361.ijkplayer.listener.OnShowThumbnailListener;
+import com.dou361.ijkplayer.widget.PlayStateParams;
+import com.dou361.ijkplayer.widget.PlayerView;
+
+
+public class PlayerActivity extends Activity {
+
+    private PlayerView player;
+    private Context mContext;
+    private View rootView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.mContext = this;
+        rootView = getLayoutInflater().from(this).inflate(R.layout.simple_player_view_player, null);
+        setContentView(rootView);
+        String url = "rtmp://192.168.1.106/live/demo";
+        player = new PlayerView(this, rootView)
+                .setTitle("什么")
+                .setScaleType(PlayStateParams.fitparent)
+                .forbidTouch(false)
+                .hideMenu(true)
+                .showThumbnail(new OnShowThumbnailListener() {
+                    @Override
+                    public void onShowThumbnail(ImageView ivThumbnail) {
+                        Glide.with(mContext)
+                                .load("http://pic2.nipic.com/20090413/406638_125424003_2.jpg")
+                                .placeholder(R.color.cl_default)
+                                .error(R.color.cl_error)
+                                .into(ivThumbnail);
+                    }
+                })
+                .setPlaySource(url)
+                .setPlayerBackListener(new OnPlayerBackListener() {
+                    @Override
+                    public void onPlayerBack() {
+                        //这里可以简单播放器点击返回键
+                        finish();
+                    }
+                })
+                .startPlay();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (player != null) {
+            player.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (player != null) {
+            player.onResume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (player != null) {
+            player.onDestroy();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (player != null) {
+            player.onConfigurationChanged(newConfig);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (player != null && player.onBackPressed()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+
+}
---
+++ app/src/main/java/com/dou361/jjdxm_ijkplayer/module/ApiService.java
@@ -0,0 +1,21 @@
+package com.dou361.jjdxm_ijkplayer.module;
+
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+
+public interface ApiService {
+
+    /**
+     * 直播相关接口
+     */
+    @FormUrlEncoded
+    @POST("/kkgame/entrance")
+    Call<String> live(@Field("parameter") String params);
+
+
+}
